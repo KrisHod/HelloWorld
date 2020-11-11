0103b9109261d410eccb36395a7d5ff7d637361a
Kristina <krisgodom@gmail.com>
2020-11-11 16:34:29 -0400

    My new Java project

---
+++ src/com/go2it/edu/CustomizedHelloWorldClass.java
@@ -0,0 +1,7 @@
+package com.go2it.edu;
+
+public class CustomizedHelloWorldClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World!" + " This is the first program of Kristina");
+    }
+}
---
+++ src/com/go2it/edu/HelloWorldClassWithCmdArgs.java
@@ -0,0 +1,7 @@
+package com.go2it.edu;
+
+public class HelloWorldClassWithCmdArgs {
+    public static void main(String[] args) {
+        System.out.println("Hello World!" + " This is the first program of " + args[0]);
+    }
+}
