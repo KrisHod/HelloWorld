2dc58bb5efc00186d472e6893329da4c2d796b21
Kristina <krisgodom@gmail.com>
2020-11-12 20:23:45 -0400

    debug1

--- src/com/go2it/edu/HelloWorldClass.java
+++ src/com/go2it/edu/HelloWorldClass.java
@@ -1,9 +1,10 @@
 package com.go2it.edu;
-
+import java.time.LocalDate;
 public class HelloWorldClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("I, Kristina Hodomska, wrote my first program on 2020-11-12");
+        LocalDate date = LocalDate.now();
+        System.out.println("I, Kristina Hodomska, wrote my first program on " + date);
         System.out.println("Programming is as easy as 31+88+9.2-90/3 that results in " + (31+88+9.2-90/3));
     }
 
